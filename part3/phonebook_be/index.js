2f8b1a4ec8fdf44633601e66d1a434766be54804
olukoluu <olukoluu@gmail.com>
2025-10-19 06:53:47 +0100

    updated exercises

--- part3/phonebook_be/index.js
+++ part3/phonebook_be/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
 const PORT = 3001;
 
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json());
 
 const persons = [
